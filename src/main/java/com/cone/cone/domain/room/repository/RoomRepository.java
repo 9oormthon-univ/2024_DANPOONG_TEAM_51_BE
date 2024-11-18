59c32f5e047233362a739ae4d3c42c0605c54f9e
fine-pine <lsm3645@g.skku.edu>
2024-11-19 03:14:14 +0900

    [FEAT] 멘토 멘티 ID로 Room 목록 찾기 추가

--- src/main/java/com/cone/cone/domain/room/repository/RoomRepository.java
+++ src/main/java/com/cone/cone/domain/room/repository/RoomRepository.java
@@ -3,5 +3,9 @@ package com.cone.cone.domain.room.repository;
 import com.cone.cone.domain.room.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoomRepository extends JpaRepository<Room, Long> {
+    List<Room> findAllByMentor_Id(Long mentorId);
+    List<Room> findAllByMentee_Id(Long menteeId);
 }
