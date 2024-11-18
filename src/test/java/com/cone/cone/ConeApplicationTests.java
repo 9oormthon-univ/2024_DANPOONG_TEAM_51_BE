62c6fbc2788136dfa7b3a2bba889b02821ac159d
sung-silver <jinna0319@gmail.com>
2024-11-18 18:01:36 +0900

    [INIT] initial commit

---
+++ src/test/java/com/cone/cone/ConeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cone.cone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
