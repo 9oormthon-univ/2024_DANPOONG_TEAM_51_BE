cd98cbfa583f322cc3ae088e43347a45db174c8e
sung-silver <jinna0319@gmail.com>
2024-11-18 22:00:05 +0900

    [FEAT] User 엔티티 구현

---
+++ src/main/java/com/cone/cone/domain/user/entity/User.java
@@ -0,0 +1,16 @@
+package com.cone.cone.domain.user.entity;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false) @Enumerated(value = EnumType.STRING)
+    private Role role;
+    @Column(nullable = false) @Enumerated(value = EnumType.STRING)
+    private PlatformType platformType;
+    private String platformId;
+}
