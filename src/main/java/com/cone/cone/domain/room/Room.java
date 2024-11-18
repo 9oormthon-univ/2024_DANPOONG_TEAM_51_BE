2ea8730a8b09faf41258be80612b4e6bbfc4ad23
fine-pine <lsm3645@g.skku.edu>
2024-11-19 01:47:00 +0900

    [FEAT] Room 엔티티 추가

---
+++ src/main/java/com/cone/cone/domain/room/Room.java
@@ -0,0 +1,25 @@
+package com.cone.cone.domain.room;
+
+import com.cone.cone.domain.user.entity.Mentee;
+import com.cone.cone.domain.user.entity.Mentor;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "rooms")
+@Getter
+public class Room {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "mentor_id", nullable = false, updatable = false)
+    private Mentor mentor;
+
+    @ManyToOne
+    @Column(name = "mentee_id", nullable = false, updatable = false)
+    private Mentee mentee;
+}
