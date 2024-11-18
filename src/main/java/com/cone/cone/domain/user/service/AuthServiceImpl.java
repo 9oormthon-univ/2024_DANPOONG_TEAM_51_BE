86027d2c5a00c62df92db8d06e908629cad86614
sung-silver <jinna0319@gmail.com>
2024-11-18 22:27:18 +0900

    [FEAT] AuthService 인터페이스 정의

---
+++ src/main/java/com/cone/cone/domain/user/service/AuthServiceImpl.java
@@ -0,0 +1,14 @@
+package com.cone.cone.domain.user.service;
+
+import com.cone.cone.domain.user.dto.request.*;
+import com.cone.cone.domain.user.dto.response.*;
+import lombok.*;
+import org.springframework.stereotype.*;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+    public LoginResponse login(LoginRequest request) {
+        return null;
+    }
+}
