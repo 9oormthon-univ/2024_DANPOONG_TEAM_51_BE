0d153cb5918b0b90bfc645b7d29f785a8b895654
fine-pine <lsm3645@g.skku.edu>
2024-11-19 01:47:48 +0900

    [FEAT] Message 엔티티 구현

---
+++ src/main/java/com/cone/cone/domain/messages/Message.java
@@ -0,0 +1,26 @@
+package com.cone.cone.domain.messages;
+
+import com.cone.cone.domain.room.Room;
+import com.cone.cone.domain.user.entity.User;
+import jakarta.persistence.*;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "messages")
+public class Message {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id", nullable = false, updatable = false)
+    private Room room;
+
+    @ManyToOne
+    @Column(name = "sender_id", nullable = false, updatable = false)
+    private User sender;
+
+    @Column(nullable = false, updatable = false)
+    private String content;
+}
