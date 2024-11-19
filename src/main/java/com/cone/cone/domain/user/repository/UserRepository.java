3e8c66f782d3647b7fd2b0bcbf767f841ed20e32
sung-silver <jinna0319@gmail.com>
2024-11-20 00:58:37 +0900

    [FEAT] UserRepository에 findByIdOrThrow 구현

--- src/main/java/com/cone/cone/domain/user/repository/UserRepository.java
+++ src/main/java/com/cone/cone/domain/user/repository/UserRepository.java
@@ -1,9 +1,16 @@
 package com.cone.cone.domain.user.repository;
 
+import static com.cone.cone.global.code.CommonExceptionCode.AUTHENTICATION_REQUIRED;
+
 import com.cone.cone.domain.user.entity.*;
+import com.cone.cone.global.exception.*;
 import java.util.*;
 import org.springframework.data.jpa.repository.*;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByPlatformId(final String platformId);
+
+    default User findByIdOrThrow(final Long userId) {
+        return findById(userId).orElseThrow(() -> new CustomException(AUTHENTICATION_REQUIRED));
+    }
 }
