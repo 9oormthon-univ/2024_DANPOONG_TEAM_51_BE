fa94e1608bfe611d96b9911b51c7aa4858292a75
sung-silver <jinna0319@gmail.com>
2024-11-20 02:49:09 +0900

    [FEAT] Mentee Profile 조회 API 구현

--- src/main/java/com/cone/cone/domain/user/controller/MenteeController.java
+++ src/main/java/com/cone/cone/domain/user/controller/MenteeController.java
@@ -3,12 +3,10 @@ package com.cone.cone.domain.user.controller;
 import com.cone.cone.domain.room.entity.Room;
 import com.cone.cone.domain.room.service.RoomService;
 import com.cone.cone.domain.user.dto.response.*;
+import com.cone.cone.domain.user.service.*;
 import com.cone.cone.global.response.ResponseTemplate;
-import jakarta.validation.*;
-import jakarta.websocket.server.PathParam;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 import static com.cone.cone.domain.room.code.RoomSuccessCode.SUCCESS_GET_ROOMS;
+import static com.cone.cone.domain.user.code.MenteeSuccessCode.SUCCESS_GET_MENTEE_PROFILE;
 
 @RestController
 @RequestMapping("/mentees")
 @RequiredArgsConstructor
 public class MenteeController implements MenteeApi {
     private final RoomService roomService;
+    private final MenteeService menteeService;
 
     @GetMapping("/{id}/rooms")
     public ResponseEntity<ResponseTemplate<List<Room>>> getRoomsById(@PathVariable Long id) {
@@ -32,6 +32,7 @@ public class MenteeController implements MenteeApi {
 
     @GetMapping("/{menteeId}")
     public ResponseEntity<ResponseTemplate<MenteeProfileResponse>> getMenteeProfile(@PathVariable("menteeId") Long menteeId) {
-        return null;
+        val response = menteeService.getMentorProfile(menteeId);
+        return ResponseEntity.ok(ResponseTemplate.success(SUCCESS_GET_MENTEE_PROFILE, response));
     }
 }
