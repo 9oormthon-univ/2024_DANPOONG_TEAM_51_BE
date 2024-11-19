c46c285d4087b3fb8cd44fcd0839910701bed504
fine-pine <lsm3645@g.skku.edu>
2024-11-19 14:12:25 +0900

    [FEAT] MentorApi.getRoomsById, MenteeAip.getRoomsById 인터페이스 정의

---
+++ src/main/java/com/cone/cone/domain/user/controller/MenteeApi.java
@@ -0,0 +1,11 @@
+package com.cone.cone.domain.user.controller;
+
+import com.cone.cone.domain.room.entity.Room;
+import com.cone.cone.global.response.ResponseTemplate;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface MenteeApi {
+    ResponseEntity<ResponseTemplate<List<Room>>> getRoomsById(Long id);
+}
