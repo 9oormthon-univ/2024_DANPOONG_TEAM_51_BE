2a9a7210070a4193aeb4d70e031a0fb55c998f95
sung-silver <jinna0319@gmail.com>
2024-11-20 01:52:19 +0900

    [FEAT] MentorProfileResponse에 from 메서드 구현

--- src/main/java/com/cone/cone/domain/user/dto/response/MentorProfileResponse.java
+++ src/main/java/com/cone/cone/domain/user/dto/response/MentorProfileResponse.java
@@ -5,10 +5,14 @@ import com.cone.cone.domain.user.entity.*;
 public record MentorProfileResponse(
         long mentorId,
         AuditStatus auditStatus,
-        String profileUrl,
+        String profileImgUrl,
         String name,
         String keyword,
         String resume,
         String introduction
 ) {
+    public static MentorProfileResponse from(Mentor mentor) {
+        return new MentorProfileResponse(mentor.getId(), mentor.getAuditStatus(), mentor.getProfileImgUrl(),
+                mentor.getUsername(), mentor.getKeyword(), mentor.getResume(), mentor.getIntroduction());
+    }
 }
