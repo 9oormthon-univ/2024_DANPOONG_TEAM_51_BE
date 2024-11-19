8410f81ff9ce5d2c64b8441940665ef4f966291e
fine-pine <lsm3645@g.skku.edu>
2024-11-20 04:47:16 +0900

    [FEAT] SocketIOController 구현

---
+++ src/main/java/com/cone/cone/external/socket/controller/SocketIOController.java
@@ -0,0 +1,54 @@
+package com.cone.cone.external.socket.controller;
+
+import com.cone.cone.domain.messages.entity.Message;
+import com.cone.cone.external.socket.ChatFacade;
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.listener.DataListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.ConnectListener;
+import com.corundumstudio.socketio.listener.DisconnectListener;
+
+import static com.cone.cone.external.socket.constant.SocketIOEvent.MESSAGE;
+
+@Component
+@Slf4j
+public class SocketIOController {
+   private final ChatFacade chatFacade;
+
+   public SocketIOController(SocketIOServer server, ChatFacade chatFacade) {
+      this.chatFacade = chatFacade;
+
+      server.addConnectListener(onConnected());
+      server.addDisconnectListener(onDisconnected());
+      server.addEventListener(MESSAGE, Message.class, onMessage());
+   }
+
+   private DataListener<Message> onMessage() {
+      return (client, data, ack) -> {
+         String room = client.getHandshakeData().getSingleUrlParam("room");
+         log.info("Event[{}] from Socket Id[{}] - Sender[{}]: {}", MESSAGE, client.getSessionId(), data.getSender(), data.getContent());
+         chatFacade.sendMessage(client, Long.parseLong(room), data);
+      };
+   }
+
+   private ConnectListener onConnected() {
+      return (client) -> {
+         String room = client.getHandshakeData().getSingleUrlParam("room");
+         log.info("Socket ID[{}] - Connected to chat module through", client.getSessionId());
+         client.joinRoom(room);
+         log.info("Room[{}]'s Participants: {}", room, client.getNamespace().getRoomOperations(room).getClients().stream().map(SocketIOClient::getSessionId)
+         );
+      };
+   }
+
+   private DisconnectListener onDisconnected() {
+      return client -> {
+         String room = client.getHandshakeData().getSingleUrlParam("room");
+         client.leaveRoom(room);
+         log.info("Socket ID[{}] - Disconnected to chat module through", client.getSessionId());
+      };
+   }
+}
