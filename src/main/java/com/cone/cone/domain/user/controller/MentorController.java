082f071f3637d747e7345a3990d84a4da5180e9d
sung-silver <jinna0319@gmail.com>
2024-11-20 01:41:28 +0900

    [FEAT] MentorController에 MentorApi implements

--- src/main/java/com/cone/cone/domain/user/controller/MentorController.java
+++ src/main/java/com/cone/cone/domain/user/controller/MentorController.java
@@ -2,7 +2,9 @@ package com.cone.cone.domain.user.controller;
 
 import com.cone.cone.domain.room.entity.Room;
 import com.cone.cone.domain.room.service.RoomService;
+import com.cone.cone.domain.user.dto.response.*;
 import com.cone.cone.global.response.ResponseTemplate;
+import jakarta.validation.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,12 +19,17 @@ import static com.cone.cone.domain.room.code.RoomSuccessCode.SUCCESS_GET_ROOMS;
 @RestController
 @RequestMapping("/mentors")
 @RequiredArgsConstructor
-public class MentorController {
+public class MentorController implements MentorApi{
     private final RoomService roomService;
 
     @GetMapping("/{id}/rooms")
     public ResponseEntity<ResponseTemplate<List<Room>>> getRoomsById(@PathVariable Long id) {
         final List<Room> rooms = roomService.getRoomsByMentorId(id);
         return ResponseEntity.ok(ResponseTemplate.success(SUCCESS_GET_ROOMS, rooms));
+    }
+
+    @GetMapping("/{mentorId}")
+    public ResponseEntity<ResponseTemplate<MentorProfileResponse>> getMentorProfile(@Valid @PathVariable("mentorId") Long mentorId) {
+        return null;
     }
 }
