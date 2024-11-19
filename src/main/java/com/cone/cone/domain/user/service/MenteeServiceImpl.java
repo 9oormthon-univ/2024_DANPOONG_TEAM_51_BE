8a7434c00a0d0c186626b49cc28478734a43d0ff
sung-silver <jinna0319@gmail.com>
2024-11-20 02:46:12 +0900

    [FEAT] 멘티 프로필 조회 서비스 로직 인터페이스 정의

---
+++ src/main/java/com/cone/cone/domain/user/service/MenteeServiceImpl.java
@@ -0,0 +1,18 @@
+package com.cone.cone.domain.user.service;
+
+import com.cone.cone.domain.user.dto.response.*;
+import com.cone.cone.domain.user.repository.*;
+import lombok.*;
+import org.springframework.stereotype.*;
+import org.springframework.transaction.annotation.*;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MenteeServiceImpl implements MenteeService{
+    private MenteeRepository menteeRepository;
+
+    public MenteeProfileResponse getMentorProfile(final Long menteeId) {
+        return null;
+    }
+}
