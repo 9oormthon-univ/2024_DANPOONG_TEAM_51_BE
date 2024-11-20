4b1a8b15180a2b7b86f36c61215fa64b1d30f163
sung-silver <jinna0319@gmail.com>
2024-11-20 17:02:23 +0900

    [FEAT] MentoringIdResponse 추가

--- src/main/java/com/cone/cone/domain/mentorings/controller/MentoringApi.java
+++ src/main/java/com/cone/cone/domain/mentorings/controller/MentoringApi.java
@@ -1,10 +1,11 @@
 package com.cone.cone.domain.mentorings.controller;
 
 import com.cone.cone.domain.mentorings.dto.request.*;
+import com.cone.cone.domain.mentorings.dto.response.*;
 import com.cone.cone.global.response.*;
 import jakarta.validation.*;
 import org.springframework.http.*;
 
 public interface MentoringApi {
-    public ResponseEntity<ResponseTemplate> requestMentoring(@Valid MentoringRequest request);
+    public ResponseEntity<ResponseTemplate<MentoringIdResponse>> requestMentoring(@Valid MentoringRequest request);
 }
--- src/main/java/com/cone/cone/domain/mentorings/controller/MentoringController.java
+++ src/main/java/com/cone/cone/domain/mentorings/controller/MentoringController.java
@@ -1,12 +1,13 @@
 package com.cone.cone.domain.mentorings.controller;
 
 import com.cone.cone.domain.mentorings.dto.request.*;
+import com.cone.cone.domain.mentorings.dto.response.*;
 import com.cone.cone.global.response.*;
 import org.springframework.http.*;
 
 public class MentoringController implements MentoringApi {
     @Override
-    public ResponseEntity<ResponseTemplate> requestMentoring(MentoringRequest request) {
+    public ResponseEntity<ResponseTemplate<MentoringIdResponse>> requestMentoring(MentoringRequest request) {
         return null;
     }
 }
