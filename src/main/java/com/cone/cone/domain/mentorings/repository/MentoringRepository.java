47a6ba5f671f62c2fd3d1e70983415346482d0fc
sung-silver <jinna0319@gmail.com>
2024-11-20 16:59:13 +0900

    [FEAT] MentoringRepository 구현

---
+++ src/main/java/com/cone/cone/domain/mentorings/repository/MentoringRepository.java
@@ -0,0 +1,7 @@
+package com.cone.cone.domain.mentorings.repository;
+
+import com.cone.cone.domain.mentorings.entity.*;
+import org.springframework.data.jpa.repository.*;
+
+public interface MentoringRepository extends JpaRepository<Long, Mentoring> {
+}
