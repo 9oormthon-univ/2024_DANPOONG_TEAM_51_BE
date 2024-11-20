917027204e0d29b16cf8ec6b86baf7ead034b883
sung-silver <jinna0319@gmail.com>
2024-11-20 17:14:13 +0900

    [FEAT] Room에 isStable 필드 추가 및 unique 제약조건 추가

--- src/main/java/com/cone/cone/domain/room/entity/Room.java
+++ src/main/java/com/cone/cone/domain/room/entity/Room.java
@@ -3,13 +3,20 @@ package com.cone.cone.domain.room.entity;
 import com.cone.cone.domain.user.entity.Mentee;
 import com.cone.cone.domain.user.entity.Mentor;
 import jakarta.persistence.*;
+import jakarta.persistence.Table;
 import lombok.*;
+import org.hibernate.annotations.*;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "rooms")
+@Table(
+        name = "rooms",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "mentor_mentee_unique", columnNames = {"mentor_id", "mentee_id"})
+        }
+)
 @Getter
 public class Room {
     @Id
@@ -24,9 +31,17 @@ public class Room {
     @JoinColumn(name = "mentee_id", nullable = false, updatable = false)
     private Mentee mentee;
 
+    @ColumnDefault("false")
+    private Boolean isStable;
+
     @Builder
     private Room(Mentor mentor, Mentee mentee) {
         this.mentor = mentor;
         this.mentee = mentee;
+    }
+
+    public boolean markAsStable() {
+        this.isStable = true;
+        return true;
     }
 }
