33776a5a7fd3f82c0f4c3409619c8352db7a8e98
fine-pine <lsm3645@g.skku.edu>
2024-11-20 18:03:15 +0900

    [FEAT] Signaling SocketIOEvent 추가

--- src/main/java/com/cone/cone/external/socket/constant/SocketIOEvent.java
+++ src/main/java/com/cone/cone/external/socket/constant/SocketIOEvent.java
@@ -2,4 +2,7 @@ package com.cone.cone.external.socket.constant;
 
 public class SocketIOEvent {
     public static final String MESSAGE = "message";
+    public static final String OFFER = "offer";
+    public static final String ANSWER = "answer";
+    public static final String CANDIDATE = "candidate";
 }
