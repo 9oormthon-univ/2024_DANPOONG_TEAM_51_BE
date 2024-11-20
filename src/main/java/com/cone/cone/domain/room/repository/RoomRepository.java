a54f46cf3e03db68828747d0ba523d5a8a0e365a
sung-silver <jinna0319@gmail.com>
2024-11-20 17:18:55 +0900

    [FEAT] findByMenteeIdAndMentorId 구현

--- src/main/java/com/cone/cone/domain/room/repository/RoomRepository.java
+++ src/main/java/com/cone/cone/domain/room/repository/RoomRepository.java
@@ -3,12 +3,14 @@ package com.cone.cone.domain.room.repository;
 import com.cone.cone.domain.room.entity.Room;
 import com.cone.cone.domain.user.entity.Mentee;
 import com.cone.cone.domain.user.entity.Mentor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
+import java.util.*;
+import org.springframework.data.jpa.repository.*;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
     boolean existsByMentorAndMentee(Mentor mentor, Mentee mentee);
     List<Room> findAllByMentor(Mentor mentor);
     List<Room> findAllByMentee(Mentee mentee);
+
+    @Query("select r from Room r where r.mentee.id = :menteeId and r.mentor.id = :mentorId")
+    Optional<Room> findByMenteeIdAndMentorId(Long menteeId, Long mentorId);
 }
