f81d56fe0ee32b890339ce7d55a09ce7786e1142
sung-silver <jinna0319@gmail.com>
2024-11-20 20:32:59 +0900

    [FEAT] RoomRepository에 findByMenteeAndMentor 구현

--- src/main/java/com/cone/cone/domain/room/repository/RoomRepository.java
+++ src/main/java/com/cone/cone/domain/room/repository/RoomRepository.java
@@ -8,9 +8,10 @@ import org.springframework.data.jpa.repository.*;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
     boolean existsByMentorAndMentee(Mentor mentor, Mentee mentee);
+
     List<Room> findAllByMentor(Mentor mentor);
+
     List<Room> findAllByMentee(Mentee mentee);
 
-    @Query("select r from Room r where r.mentee.id = :menteeId and r.mentor.id = :mentorId")
-    Optional<Room> findByMenteeIdAndMentorId(Long menteeId, Long mentorId);
+    Optional<Room> findByMenteeAndMentor(Mentee mentee, Mentor mentor);
 }
