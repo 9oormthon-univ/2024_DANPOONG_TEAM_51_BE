5e98f4a9c85b0a753101d3ff1f40ecf48711433b
fine-pine <lsm3645@g.skku.edu>
2024-11-22 00:52:23 +0900

    [FEAT] MentoringStatus 변경 및 주석 추가

--- src/main/java/com/cone/cone/domain/mentorings/entity/MentoringStatus.java
+++ src/main/java/com/cone/cone/domain/mentorings/entity/MentoringStatus.java
@@ -1,5 +1,14 @@
 package com.cone.cone.domain.mentorings.entity;
 
 public enum MentoringStatus {
-   REVIEW, APPROVED, REJECTED, DONE, SUMMARIZED
+   // 멘토가 신청을 검토 중
+   INREVIEW,
+   // 멘토가 신청을 수락, 멘토링 진행 전
+   APPROVED,
+   // 멘토가 신청을 거절
+   REJECTED,
+   // 멘토링 진행 후, 요약본 전달 전
+   DONE,
+   // 요약본 전달 후
+   SUMMARIZED
 }
