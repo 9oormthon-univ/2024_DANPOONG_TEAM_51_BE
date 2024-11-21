88bc74707ce347500b403d995d281d9eb69cb6c1
fine-pine <lsm3645@g.skku.edu>
2024-11-22 01:05:23 +0900

    [FEAT] User.keywords -> Mentor/Mentee 로 이동

--- src/main/java/com/cone/cone/domain/user/entity/Mentee.java
+++ src/main/java/com/cone/cone/domain/user/entity/Mentee.java
@@ -2,7 +2,11 @@ package com.cone.cone.domain.user.entity;
 
 import jakarta.persistence.*;
 import java.util.*;
+import java.util.stream.Collectors;
+
 import lombok.*;
+
+import static com.cone.cone.global.constant.DomainConstant.COMMA;
 
 @Entity
 @Table(name = "mentees")
@@ -18,8 +22,12 @@ public class Mentee {
     private User user;
 
     private String concernSummary;
-    private String mentorPreference;
+
     private String concernDetail;
+
+    private String mentorPreference;
+
+    private String keywords;
 
     @Builder
     private Mentee(User user) {
@@ -35,6 +43,8 @@ public class Mentee {
     }
 
     public List<String> getKeywords() {
-        return user.getKeywords();
+        return Arrays.stream(keywords.split(COMMA))
+                .map(String::trim)  // 각 항목에서 불필요한 공백을 제거
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/cone/cone/domain/user/entity/User.java
+++ src/main/java/com/cone/cone/domain/user/entity/User.java
@@ -15,32 +15,30 @@ import lombok.*;
 public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false) @Enumerated(value = EnumType.STRING)
-    private Role role;
+
+    private String username;
+
+    private String profileImgUrl;
+
     @Column(nullable = false) @Enumerated(value = EnumType.STRING)
     private PlatformType platformType;
+
     @Column(nullable = false, unique = true)
     private String platformId;
-    private String username;
-    private String profileImgUrl;
-    private String keywords; // 온보딩에서 입력받을 수 없어 임시로 nullable하게 설정
+
+    @Column(nullable = false) @Enumerated(value = EnumType.STRING)
+    private Role role;
 
     @Builder
     private User(Role role, PlatformType platformType, String platformId, String username, String profileImgUrl) {
-        this.role = role;
-        this.platformType = platformType;
-        this.platformId = platformId;
         this.username = username;
         this.profileImgUrl = profileImgUrl;
+        this.platformType = platformType;
+        this.platformId = platformId;
+        this.role = role;
     }
 
     public void changeRole(final Role role) {
         this.role = role;
-    }
-
-    public List<String> getKeywords() {
-        return Arrays.stream(keywords.split(COMMA))
-                .map(String::trim)  // 각 항목에서 불필요한 공백을 제거
-                .collect(Collectors.toList());
     }
 }
