c9c52011b370eefac33e257742cac055bf93ad97
fine-pine <lsm3645@g.skku.edu>
2024-11-22 01:58:50 +0900

    [FEAT] AuthService.getUserById 인터페이스 정의

--- src/main/java/com/cone/cone/domain/auth/service/AuthService.java
+++ src/main/java/com/cone/cone/domain/auth/service/AuthService.java
@@ -2,10 +2,13 @@ package com.cone.cone.domain.auth.service;
 
 import com.cone.cone.domain.user.dto.request.*;
 import com.cone.cone.domain.user.dto.response.*;
+import com.cone.cone.domain.user.entity.User;
 import jakarta.servlet.http.*;
 
 public interface AuthService {
     RoleResponse login(HttpServletRequest httpServletRequest, LoginRequest request);
 
     RoleResponse changeRole(HttpServletRequest httpServletRequest, Long userId, RoleRequest request);
+
+    User getUserById(Long id);
 }
