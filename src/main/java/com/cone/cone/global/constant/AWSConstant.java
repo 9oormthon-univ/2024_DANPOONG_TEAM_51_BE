f1a6bcf1e29e9066640c7817e17303662d8e3e7a
sung-silver <jinna0319@gmail.com>
2024-11-22 17:46:02 +0900

    [FEAT] AWS에서 사용되는 상수 분리

---
+++ src/main/java/com/cone/cone/global/constant/AWSConstant.java
@@ -0,0 +1,8 @@
+package com.cone.cone.global.constant;
+
+public abstract class AWSConstant {
+    public static final String AWS_ACCESS_KEY_ID = "aws.accessKeyId";
+    public static final String AWS_SECRET_ACCESS_KEY = "aws.secretAccessKey";
+    public static final Long PRE_SIGNED_URL_EXPIRE_MINUTE = 1L;
+    public static final String VOICE_FILE_FORMAT = ".webm";
+}
