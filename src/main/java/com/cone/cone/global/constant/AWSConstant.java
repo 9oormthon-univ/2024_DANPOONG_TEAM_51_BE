2556a038f9353b8cb995ca335587b381aa69a45b
sung-silver <jinna0319@gmail.com>
2024-11-22 22:49:23 +0900

    [CHORE] url 유효시간 변경

--- src/main/java/com/cone/cone/global/constant/AWSConstant.java
+++ src/main/java/com/cone/cone/global/constant/AWSConstant.java
@@ -3,7 +3,7 @@ package com.cone.cone.global.constant;
 public abstract class AWSConstant {
     public static final String AWS_ACCESS_KEY_ID = "aws.accessKeyId";
     public static final String AWS_SECRET_ACCESS_KEY = "aws.secretAccessKey";
-    public static final Long PRE_SIGNED_URL_EXPIRE_MINUTE = 1L;
+    public static final Long PRE_SIGNED_URL_EXPIRE_MINUTE = 10L;
     public static final String VOICE_FILE_FORMAT = ".webm";
     public static final String MENTORING_ORIGIN = "/mentoring-original";
     public static final String MENTORING_SUMMARY = "/mentoring-summary";
