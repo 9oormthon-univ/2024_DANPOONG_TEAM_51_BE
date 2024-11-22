71a6bc3aa990752fe1c77dd25929c09f3c08c96e
fine-pine <lsm3645@g.skku.edu>
2024-11-22 22:27:38 +0900

    [FEAT] MentoringApi 인터페이스 정의

--- src/main/java/com/cone/cone/domain/mentorings/controller/MentoringApi.java
+++ src/main/java/com/cone/cone/domain/mentorings/controller/MentoringApi.java
@@ -8,4 +8,7 @@ import org.springframework.http.*;
 
 public interface MentoringApi {
     ResponseEntity<ResponseTemplate<MentoringIdResponse>> requestMentoring(Long menteeId, @Valid MentoringRequest request);
+    ResponseEntity<ResponseTemplate<MentoringTimeResponse>> bookingMentoring(Long userId, @Valid MentoringBookingRequest request);
+    ResponseEntity<ResponseTemplate<Void>> permitMentoring(Long mentorId, Long mentoringId);
+    ResponseEntity<ResponseTemplate<Void>> rejectMentoring(Long mentorId, Long mentoringId);
 }
