1ee50a22ebda6758bc2d8acf9d07acdcab1d2b64
fine-pine <lsm3645@g.skku.edu>
2024-11-23 01:21:21 +0900

    [FEAT] BaseTime 생성자 추가

--- src/main/java/com/cone/cone/global/base/BaseTime.java
+++ src/main/java/com/cone/cone/global/base/BaseTime.java
@@ -3,17 +3,26 @@ package com.cone.cone.global.base;
 import jakarta.persistence.*;
 import java.time.*;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.*;
 import org.springframework.data.jpa.domain.support.*;
 
 @Getter
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
+@SuperBuilder
+@NoArgsConstructor
 public abstract class BaseTime {
     @CreatedDate
-    @Column(updatable = false)
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime updatedAt;
+
+    protected BaseTime(LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 }
