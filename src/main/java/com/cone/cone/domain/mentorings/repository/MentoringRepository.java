5084f9ae65db19781242eab77f87667fde9ee0b6
fine-pine <lsm3645@g.skku.edu>
2024-11-22 22:59:21 +0900

    [FEAT] MentoringRepository findAllByUserIdOrderCreatedAtDesc -> findByIdOrThrow

--- src/main/java/com/cone/cone/domain/mentorings/repository/MentoringRepository.java
+++ src/main/java/com/cone/cone/domain/mentorings/repository/MentoringRepository.java
@@ -2,10 +2,19 @@ package com.cone.cone.domain.mentorings.repository;
 
 import com.cone.cone.domain.mentorings.entity.*;
 import java.util.*;
+
+import com.cone.cone.global.exception.CustomException;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.*;
 
+import static com.cone.cone.domain.mentorings.code.MentoringExceptionCode.INVALID_REQUEST_FIND_MENTORING;
+
 public interface MentoringRepository extends JpaRepository<Mentoring, Long> {
+
+    default Mentoring findByIdOrThrow(final Long id) {
+        return findById(id).orElseThrow(() -> new CustomException(INVALID_REQUEST_FIND_MENTORING));
+    }
+
     @Query("select m from Mentoring m " +
             "join fetch m.room r " +
             "join fetch r.mentee " +
@@ -19,11 +28,4 @@ public interface MentoringRepository extends JpaRepository<Mentoring, Long> {
             "where r.mentor.id = :mentorId " +
             "order by m.createdAt desc")
     List<Mentoring> findAllByMentorIdOrderCreatedAtDesc(@Param("mentorId") Long menteeId);
-
-    @Query("select m from Mentoring m " +
-            "join fetch m.room r " +
-            "join fetch r.mentor " +
-            "where r.mentor.id = :userId or r.mentee.id = :userId " +
-            "order by m.createdAt desc")
-    List<Mentoring> findAllByUserIdOrderCreatedAtDesc(@Param("userId") Long userId);
 }
