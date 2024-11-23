440887f1e9eb25bde4efbdad9a1197d93bf3e33b
fine-pine <lsm3645@g.skku.edu>
2024-11-24 00:40:02 +0900

    [FEAT] SocketService.broadcastDataByRoomId 구현

--- src/main/java/com/cone/cone/external/socket/SocketService.java
+++ src/main/java/com/cone/cone/external/socket/SocketService.java
@@ -1,9 +1,9 @@
 package com.cone.cone.external.socket;
 
-import com.cone.cone.domain.messages.entity.Message;
 import com.corundumstudio.socketio.SocketIOClient;
 
 public interface SocketService {
     void sendData (SocketIOClient client, Long roomId, String event, Object data);
     void broadcastData (SocketIOClient client, Long roomId, String event, Object data);
+    void broadcastDataByRoomId (Long roomId, String event, Object data);
 }
